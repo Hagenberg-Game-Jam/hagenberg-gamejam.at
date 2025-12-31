55f4d24141eff0cdcca666a2091e1c3c24d590cd
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2025-12-31 15:19:09 +0100

    Added 2025 game jam
    Using the new create-jam script!

--- config/hyde.php
+++ config/hyde.php
@@ -354,7 +354,8 @@ return [
         'exclude' => [
             '404',
             '2015', '2016', '2017', '2018', '2019', '2020', '2022', '2023', '2024',
-            'people',
+            'people',            '2025',
+
         ],
 
         // Any extra links you want to add to the navigation menu can be added here.
